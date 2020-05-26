ab192a4b307049f9e150d523d2dc52aea7372762
Your Name <zzafachreza@gmail.com>
2020-05-26 13:54:48 +0700

    mengganti port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var server = http.createServer(function(req,res){
 
 	res.end("selamat datang di Aplikasi Pertamaku..");
 
-}).listen(8000);
+}).listen(80);
 
 console.log("Server is running on port 8000");
